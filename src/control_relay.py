f64e641ce4bf64efbae98fb8a4d8e9a5a24f7a46
andi Klein <pabloemma@gmail.com>
2022-10-01 15:59:26 -0600

    relay control class, very simple

---
+++ src/control_relay.py
@@ -0,0 +1,27 @@
+import usbrelay_py
+
+class MyRelay(object):
+
+    def __init__(self):
+
+        #this initializes the usb relay
+        count = usbrelay_py.board_count()
+        print("Count: ",count)
+
+        boards = usbrelay_py.board_details()
+        print("Boards: ",boards)
+
+
+    def SetRelayOn(self,board):
+        relay = 1
+        result = usbrelay_py.board_control(board,),relay,1)
+        return
+
+    def SetRelayOff(self,board):
+        relay = 0
+        result = usbrelay_py.board_control(board,),relay,1)
+        return
+
+if __name__ == "__main__":
+
+    MR = MyRelay()
