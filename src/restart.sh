b2788d2cb313419c2c9de02485811bd78458ddd1
andi Klein <pabloemma@gmail.com>
2022-10-03 14:44:02 -0600

    restart script

---
+++ src/restart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+ 
+
+
+if [ $(ps -efa | grep -v grep | grep Tmeasure.py -c) -gt 0 ] ;
+then
+    echo "Process running ...";
+else
+#temporary if program dies download all the necessary programs
+#cd /home/pi/git/speedtest/src
+#./update_speedtest
+#cd ~
+
+python3 -u /home/pi/git/Thermostat/src/Tmeasure.py  &
+
+
+echo "Starting the process";
+fi;
