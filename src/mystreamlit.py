c7fdb46f8f8a8327bf37c3e91c909698f11af98c
andi Klein <pabloemma@gmail.com>
2022-10-07 17:25:29 -0600

    more changes to desigtn

--- src/mystreamlit.py
+++ src/mystreamlit.py
@@ -3,5 +3,6 @@ import streamlit as st
 mytemp = 'current temperature  ' + str(65)
 st.header(mytemp)
 x = st.slider('Temperature',min_value = 60, max_value=80)
-st.header(x)
+newtemp = 'the new temperature is '+str(x)
+st.header(newtemp)
 print(x)
