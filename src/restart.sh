ba6b70f2a2d2385ec0f417abe990b3ae72b0057d
andi Klein <pabloemma@gmail.com>
2022-10-08 14:12:10 -0600

    added ReadValue to the restart script

--- src/restart.sh
+++ src/restart.sh
@@ -14,5 +14,19 @@ else
 python3 -u /home/pi/git/Thermostat/src/Tmeasure.py  &
 
 
-echo "Starting the process";
+echo "Starting the process Tmeasure";
+fi;
+if [ $(ps -efa | grep -v grep | grep ReadValue.py -c) -gt 0 ] ;
+then
+    echo "Process running ...";
+else
+#temporary if program dies download all the necessary programs
+#cd /home/pi/git/speedtest/src
+#./update_speedtest
+#cd ~
+
+python3 -u /home/pi/git/Thermostat/src/ReadValue.py  &
+
+
+echo "Starting the process ReadValue";
 fi;
