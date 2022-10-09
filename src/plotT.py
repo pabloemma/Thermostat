b5c0eef22fc90717694c8cbb6047ca451d4add93
andi Klein <pabloemma@gmail.com>
2022-10-09 17:48:43 -0600

    added plt.show

--- src/plotT.py
+++ src/plotT.py
@@ -7,6 +7,14 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 
-data = pd.read_csv('/home/pi/scratch/Temperature_2022-10-09_.csv')
+data = pd.read_csv('/home/klein/scratch/Temperature_2022-10-09_.csv',index_col=0,parse_dates=True)
 temp = data['temperature']
+#temp.plot()
 temp.plot(ax=ax,style='k-')
+plt.show()
+
+
+
+
+
+
