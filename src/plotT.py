301c8fd13056943b2553147c00cecd948c666fee
andi Klein <pabloemma@gmail.com>
2022-10-09 17:27:43 -0600

    plotting

---
+++ src/plotT.py
@@ -0,0 +1,12 @@
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+data = pd.read_csv('Temperature_2022-10-09_.csv')
+temp = data['temperature']
+temp.plot(ax=ax,style='k-')
