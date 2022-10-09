bed3ec4f61a99a799e6e08e5cde4ba9c9f2d53d3
andi Klein <pabloemma@gmail.com>
2022-10-09 17:55:46 -0600

    no line connection

--- src/plotT.py
+++ src/plotT.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 
-data = pd.read_csv('/home/pi/scratch/Temperature_2022-10-09_.csv',index_col=0,parse_dates=True)
+data = pd.read_csv('/home/klein/scratch/Temperature_2022-10-09_.csv',index_col=0,parse_dates=True)
 temp = data['temperature']
 #temp.plot()
-temp.plot(ax=ax,style='k-')
+temp.plot(ax=ax,marker='*',linestyle='None')
 plt.show()
 
 
