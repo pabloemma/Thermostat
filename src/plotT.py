2ba156f365bb0210830b86eee3ff25193eca0da8
andi Klein <pabloemma@gmail.com>
2022-10-09 17:28:25 -0600

    pathh was wrong

--- src/plotT.py
+++ src/plotT.py
@@ -7,6 +7,6 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 
-data = pd.read_csv('Temperature_2022-10-09_.csv')
+data = pd.read_csv('/home/pi/scratch/Temperature_2022-10-09_.csv')
 temp = data['temperature']
 temp.plot(ax=ax,style='k-')
