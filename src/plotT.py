40d37ccc492f63d6a6ebd3fab4340cf322f88738
andi Klein <pabloemma@gmail.com>
2022-10-10 19:40:52 -0600

    convert to F

--- src/plotT.py
+++ src/plotT.py
@@ -8,7 +8,7 @@ fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 
 data = pd.read_csv('/home/klein/scratch/Temperature_2022-10-09_.csv',index_col=0,parse_dates=True)
-temp = data['temperature']
+temp = data['temperature']*1.8+32
 #temp.plot()
 temp.plot(ax=ax,marker='*',linestyle='None')
 plt.show()
