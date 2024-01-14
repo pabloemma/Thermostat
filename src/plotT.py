946a694be5e27ff07422f81fa4efbb699841d728
andi klein <pabloemma@gmail.com>
2024-01-14 07:10:27 -0700

    automatically get the right file and proper path

--- src/plotT.py
+++ src/plotT.py
@@ -1,13 +1,17 @@
-import datetime
+import datetime as dt
 import pandas as pd
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
-
-data = pd.read_csv('/Users/klein/git/Thermostat/src/Temperature_2022-12-26_.csv',index_col=0,parse_dates=True)
+a=dt.datetime.now()
+b=dt.datetime.strftime(a,"%Y-%m-%d")
+temp= 'Temperature_'+b+'_.csv'
+myfile = Path.home()+'/scratch/'+temp
+data = pd.read_csv(myfile,index_col=0,parse_dates=True)
 temp = data['temperature']*1.8+32
 #temp.plot()
 #temp.plot(ax=ax,marker='+',color='green',linestyle='None')
@@ -18,7 +22,7 @@ temp.plot(ax=ax,color='green',linestyle='--')
 
 
 
-plt.ylim(60.,80.)
+plt.ylim(60.,100.)
 
 ax.set_ylabel("Temperature")
 ax.set_title("living room T")
