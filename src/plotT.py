99eea646b1ce28ae0cb498c7e9594d97bff0c08b
andi klein <pabloemma@gmail.com>
2024-01-14 07:12:37 -0700

    corrected typo

--- src/plotT.py
+++ src/plotT.py
@@ -10,7 +10,7 @@ ax = fig.add_subplot(1,1,1)
 a=dt.datetime.now()
 b=dt.datetime.strftime(a,"%Y-%m-%d")
 temp= 'Temperature_'+b+'_.csv'
-myfile = Path.home()+'/scratch/'+temp
+myfile =str(Path.home())+'/scratch/'+temp
 data = pd.read_csv(myfile,index_col=0,parse_dates=True)
 temp = data['temperature']*1.8+32
 #temp.plot()
